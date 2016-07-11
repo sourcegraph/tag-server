aa28565fdf684c3b7dfc7819661248144ebe4d6b
Beyang Liu <beyang@sourcegraph.com>
2016-07-10 21:23:18 -0700

    ignore files and logging

--- ctags/scangraph.go
+++ ctags/scangraph.go
@@ -2,8 +2,11 @@ package ctags
 
 import (
 	"bufio"
+	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"time"
 
 	"sourcegraph.com/sourcegraph/srclib/unit"
 )
@@ -26,6 +29,8 @@ func Scan() ([]*unit.SourceUnit, error) {
 	return p.Units(), nil
 }
 
+var ignoreFiles = []string{".srclib-cache", "node_modules", "vendor"}
+
 func parse(files []string) (*ETagsParser, error) {
 	const tagsFilename = "tags"
 	args := []string{"-e", "-f", tagsFilename}
@@ -34,9 +39,19 @@ func parse(files []string) (*ETagsParser, error) {
 	} else {
 		args = append(args, files...)
 	}
+	excludeArgs := make([]string, 0, len(ignoreFiles))
+	for _, ignoreFile := range ignoreFiles {
+		excludeArgs = append(excludeArgs, fmt.Sprintf("--exclude=%s", ignoreFile))
+	}
+	args = append(args, excludeArgs...)
+
+	log.Printf("...running ctags with args %v", args)
+	ctagsStartTime := time.Now()
 	if err := exec.Command("ctags", args...).Run(); err != nil {
 		return nil, err
 	}
+	log.Printf("...done running ctags (duration: %v)", time.Since(ctagsStartTime))
+
 	tagsFile, err := os.Open(tagsFilename)
 	if err != nil {
 		return nil, err
