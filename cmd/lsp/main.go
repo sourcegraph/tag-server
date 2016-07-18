e1aed16208eeb99a2087c514e03c14371f2856fb
Beyang Liu <beyang@sourcegraph.com>
2016-07-17 19:20:35 -0700

    stdin/out transport

--- cmd/lsp/main.go
+++ cmd/lsp/main.go
@@ -2,33 +2,82 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"net/rpc"
+	"net/rpc/jsonrpc"
 	"os"
 	"strings"
 
-	"github.com/gorilla/rpc"
+	gorpc "github.com/gorilla/rpc"
 	"github.com/gorilla/rpc/json"
+	"github.com/jessevdk/go-flags"
 	"github.com/sourcegraph/tag-server/ctags"
 	"github.com/sourcegraph/tag-server/lsp"
 )
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+	cli := flags.NewNamedParser("lsp", flags.PrintErrors|flags.PassDoubleDash)
+	cli.AddCommand("serve", "", "", &ServeCmd{})
+	_, err := cli.Parse()
+	if err != nil {
+		log.Println(err)
 		os.Exit(1)
 	}
 }
 
-func run() error {
-	s := rpc.NewServer()
-	s.RegisterCodec(&codec{json.NewCodec()}, "application/json")
-	if err := s.RegisterService(lsp.NewHTTPLangService(ctags.NewLangService()), "lsp"); err != nil {
-		return err
-	}
-	http.Handle("/", s)
+type ServeCmd struct {
+	Std bool `long:"std" description:""`
+}
 
-	fmt.Println("listening on :9090")
-	http.ListenAndServe(":9090", nil)
+func (c *ServeCmd) Execute(args []string) error {
+	if c.Std {
+		if err := rpc.Register(ctags.NewLangService()); err != nil {
+			return err
+		}
+		stdPipe := ReadWriteCloser{os.Stdout, os.Stdin}
+		fmt.Println("listening on stdin")
+		rpc.ServeCodec(jsonrpc.NewServerCodec(stdPipe))
+		return nil
+	} else {
+		s := gorpc.NewServer()
+		s.RegisterCodec(&codec{json.NewCodec()}, "application/json")
+		if err := s.RegisterService(lsp.NewHTTPLangService(ctags.NewLangService()), "lsp"); err != nil {
+			return err
+		}
+		http.Handle("/", s)
+
+		fmt.Println("listening on :9090")
+		return http.ListenAndServe(":9090", nil)
+	}
+}
+
+type ReadWriteCloser struct {
+	io.ReadCloser
+	io.WriteCloser
+}
+
+func (r ReadWriteCloser) Read(p []byte) (n int, err error) {
+	return r.ReadCloser.Read(p)
+}
+
+func (r ReadWriteCloser) Write(p []byte) (n int, err error) {
+	return r.WriteCloser.Write(p)
+}
+
+func (r ReadWriteCloser) Close() error {
+	rErr := r.ReadCloser.Close()
+	wErr := r.WriteCloser.Close()
+	if rErr != nil {
+		if wErr != nil {
+			return fmt.Errorf("ReadCloser error: %s, WriteCloser error: %s", rErr, wErr)
+		}
+		return rErr
+	}
+	if wErr != nil {
+		return wErr
+	}
 	return nil
 }
 
@@ -36,10 +85,10 @@ type codec struct {
 	*json.Codec
 }
 
-var _ rpc.Codec = (*codec)(nil)
+var _ gorpc.Codec = (*codec)(nil)
 
 type codecReq struct {
-	rpc.CodecRequest
+	gorpc.CodecRequest
 }
 
 func (r *codecReq) Method() (string, error) {
@@ -56,9 +105,9 @@ func (r *codecReq) WriteResponse(w http.ResponseWriter, v interface{}, err error
 	return r.CodecRequest.WriteResponse(w, v, err)
 }
 
-var _ rpc.CodecRequest = (*codecReq)(nil)
+var _ gorpc.CodecRequest = (*codecReq)(nil)
 
-func (c *codec) NewRequest(r *http.Request) rpc.CodecRequest {
+func (c *codec) NewRequest(r *http.Request) gorpc.CodecRequest {
 	return &codecReq{c.Codec.NewRequest(r)}
 }
 
--- ctags/lsp.go
+++ ctags/lsp.go
@@ -6,10 +6,10 @@ import (
 	"github.com/sourcegraph/tag-server/lsp"
 )
 
-type langSvc struct{}
+type LangSvc struct{}
 
-func (s *langSvc) Initialize(params *lsp.InitializeParams, result *lsp.InitializeResult) error {
-	log.Printf("langSvc.Initialize(%+v)", params)
+func (s *LangSvc) Initialize(params *lsp.InitializeParams, result *lsp.InitializeResult) error {
+	log.Printf("LangSvc.Initialize(%+v)", params)
 	// vfsURL, err := url.Parse(params.RootPath)
 	// if err != nil {
 	// 	return fmt.Errorf("Could not parse VFS URL: %s", err)
@@ -19,56 +19,56 @@ func (s *langSvc) Initialize(params *lsp.InitializeParams, result *lsp.Initializ
 
 	return nil
 }
-func (s *langSvc) Completion(params *lsp.TextDocumentPositionParams, result *lsp.CompletionList) error {
+func (s *LangSvc) Completion(params *lsp.TextDocumentPositionParams, result *lsp.CompletionList) error {
 	return nil
 }
-func (s *langSvc) CompletionItemResolve(params *lsp.CompletionList, result *lsp.CompletionList) error {
+func (s *LangSvc) CompletionItemResolve(params *lsp.CompletionList, result *lsp.CompletionList) error {
 	return nil
 }
-func (s *langSvc) HoverRequest(params *lsp.TextDocumentPositionParams, result *lsp.Hover) error {
+func (s *LangSvc) HoverRequest(params *lsp.TextDocumentPositionParams, result *lsp.Hover) error {
 	return nil
 }
-func (s *langSvc) SignatureHelpRequest(params *lsp.TextDocumentPositionParams, result *lsp.SignatureHelp) error {
+func (s *LangSvc) SignatureHelpRequest(params *lsp.TextDocumentPositionParams, result *lsp.SignatureHelp) error {
 	return nil
 }
-func (s *langSvc) GoToDefinition(params *lsp.TextDocumentPositionParams, result *[]lsp.Location) error {
+func (s *LangSvc) GoToDefinition(params *lsp.TextDocumentPositionParams, result *[]lsp.Location) error {
 	// TODO
 	return nil
 }
-func (s *langSvc) FindReferences(params *lsp.ReferenceParams, result *[]lsp.Location) error {
+func (s *LangSvc) FindReferences(params *lsp.ReferenceParams, result *[]lsp.Location) error {
 	return nil
 }
-func (s *langSvc) DocumentHighlights(params *lsp.ReferenceParams, result *lsp.DocumentHighlight) error {
+func (s *LangSvc) DocumentHighlights(params *lsp.ReferenceParams, result *lsp.DocumentHighlight) error {
 	return nil
 }
-func (s *langSvc) DocumentSymbols(params *lsp.DocumentSymbolParams, result *[]lsp.SymbolInformation) error {
+func (s *LangSvc) DocumentSymbols(params *lsp.DocumentSymbolParams, result *[]lsp.SymbolInformation) error {
 	// TODO
 	return nil
 }
-func (s *langSvc) WorkplaceSymbols(params *lsp.WorkplaceSymbolParams, result *[]lsp.SymbolInformation) error {
+func (s *LangSvc) WorkplaceSymbols(params *lsp.WorkplaceSymbolParams, result *[]lsp.SymbolInformation) error {
 	return nil
 }
-func (s *langSvc) CodeAction(params *lsp.CodeActionParams, result *[]lsp.Command) error {
+func (s *LangSvc) CodeAction(params *lsp.CodeActionParams, result *[]lsp.Command) error {
 	return nil
 }
-func (s *langSvc) CodeLensRequest(params *lsp.CodeLensParams, result *[]lsp.Command) error {
+func (s *LangSvc) CodeLensRequest(params *lsp.CodeLensParams, result *[]lsp.Command) error {
 	return nil
 }
-func (s *langSvc) CodeLensResolve(params *lsp.CodeLens, result *lsp.CodeLens) error {
+func (s *LangSvc) CodeLensResolve(params *lsp.CodeLens, result *lsp.CodeLens) error {
 	return nil
 }
-func (s *langSvc) DocumentFormatting(params *lsp.DocumentFormattingParams, result *[]lsp.TextEdit) error {
+func (s *LangSvc) DocumentFormatting(params *lsp.DocumentFormattingParams, result *[]lsp.TextEdit) error {
 	return nil
 }
-func (s *langSvc) DocumentOnTypeFormatting(params *lsp.DocumentFormattingParams, result *[]lsp.TextEdit) error {
+func (s *LangSvc) DocumentOnTypeFormatting(params *lsp.DocumentFormattingParams, result *[]lsp.TextEdit) error {
 	return nil
 }
-func (s *langSvc) Rename(params *lsp.RenameParams, result *lsp.WorkspaceEdit) error {
+func (s *LangSvc) Rename(params *lsp.RenameParams, result *lsp.WorkspaceEdit) error {
 	return nil
 }
 
-var _ lsp.LangSvc = (*langSvc)(nil)
+var _ lsp.LangSvc = (*LangSvc)(nil)
 
 func NewLangService() lsp.LangSvc {
-	return &langSvc{}
+	return &LangSvc{}
 }
